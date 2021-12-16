2e4689713ff4b6aae4b0c95d05d34f16d2806190
Ellis Lunnon <ellislunnon@gmail.com>
2021-12-16 20:39:12 +0000

    Added academic integrity declaration

--- pychessbinds/piece.cpp
+++ pychessbinds/piece.cpp
@@ -1,3 +1,8 @@
+/* I, Ellis Lunnon, have read and understood the School's Academic Integrity Policy, as well as guidance relating to this  */
+/* module, and confirm that this submission complies with the policy. The content of this file is my own original work,  */
+/* with any significant material copied or adapted from other sources clearly indicated and attributed. */
+
+
 #include <iostream>
 #include "position.hpp"
 #include "piece.hpp"
